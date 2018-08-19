1be9555802e328321bf491e27e8a4c22c8aa8bf4
Angela Zhou <angelazhou922@gmail.com>
2018-08-18 22:53:31 -0400

    Changed css

--- div.js
+++ div.js
@@ -21,7 +21,7 @@ $(document).ready(function(){
     }
     
     function checkProblem () {
-        var rightAnswer = numOne * numTwo;
+        var rightAnswer = numOne + numTwo;
         var inputAnswer = $("#input").val();
         if (Number.parseFloat(inputAnswer) === rightAnswer) {
             correctScore++;
@@ -29,6 +29,7 @@ $(document).ready(function(){
         } else {
             wrongScore++;
             $("#wrongSpan").html(wrongScore);
+            alert("Wrong! Review " + numOne + " + " + numTwo + "!");
         }
         newProblem();
         $("#input").val("");
@@ -48,17 +49,22 @@ $(document).ready(function(){
             display.text(minutes + ":" + seconds);
     
             if (--timer < 0) {
-                alert("Time's up!");
+                var confirmed = confirm("Time's up!");
+                if (confirmed === true) {
+                    location.reload();
+                } else {
+                    location.reload();
+                }
             }
         }, 1000);
     }
     
     jQuery(function ($) {
-        var oneMinute = 60;
+        var timerTime = 60;
             display = $('#time');
-        startTimer(oneMinute, display);
+        display.html(timerTime);
+        startTimer(timerTime, display);
     });
-
     
     newProblem();
 })
---
+++ mult.js
@@ -0,0 +1,70 @@
+$(document).ready(function(){
+    var numOne = "";
+    var numTwo = "";
+    var correctScore = 0;
+    var wrongScore = 0;
+    $(document).keypress(function(e){
+        if (e.which === 13) { //13 is the enter key
+            checkProblem();
+        }
+    })
+
+    function randomNum () {
+        return Math.floor(Math.random()*10);
+    }
+
+    function newProblem () {
+        numOne = randomNum();
+        numTwo = randomNum();
+        $(".numberOne").html(numOne);
+        $(".numberTwo").html(numTwo);
+    }
+    
+    function checkProblem () {
+        var rightAnswer = numOne * numTwo;
+        var inputAnswer = $("#input").val();
+        if (Number.parseFloat(inputAnswer) === rightAnswer) {
+            correctScore++;
+            $("#correctSpan").html(correctScore);
+        } else {
+            wrongScore++;
+            $("#wrongSpan").html(wrongScore);
+            alert("Wrong! Review " + numOne + " x " + numTwo + "!");
+        }
+        newProblem();
+        $("#input").val("");
+    }
+
+    function startTimer(duration, display) {
+        var timer = duration
+        var minutes;
+        var seconds;
+        setInterval(function () {
+            minutes = parseInt(timer / 60);
+            seconds = parseInt(timer % 60);
+    
+            minutes = minutes < 10 ? "0" + minutes : minutes;
+            seconds = seconds < 10 ? "0" + seconds : seconds;
+    
+            display.text(minutes + ":" + seconds);
+    
+            if (--timer < 0) {
+                var confirmed = confirm("Time's up!");
+                if (confirmed === true) {
+                    location.reload();
+                } else {
+                    location.reload();
+                }
+            }
+        }, 1000);
+    }
+    
+    jQuery(function ($) {
+        var timerTime = 60;
+            display = $('#time');
+        display.html(timerTime);
+        startTimer(timerTime, display);
+    });
+    
+    newProblem();
+})
